70ce60cc2db1afbbb2c26a385c4bcc36b811dd4c
Ayushman Tiwari <ayushman5022@gmail.com>
2024-03-09 01:33:35 +0530

    Auth routes

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,34 @@
 const express = require("express");
 const dbconnect = require('./db/db_connect');
-
+import cookieParser from "cookie-parser";
 const app = express();
+
+import authRoutes from "./routes/Auth.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 // it is used to import data from .env file
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
+app.use(cookieParser());
 
-
-// start server on Port
-app.listen(PORT, () => {
-    console.log(`Server successfully started at ${PORT}`);
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // connnecting database
 dbconnect();
 
-// default route
-app.use("/", (req, res) => {
-    res.send('<h1> This is homepage baby</h1>');
-});
+app.get("/", (req,res)=>{
+    return res.json({
+        success:true,
+        message:"Your server is up and running"
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`App is running at port ${PORT}`)
+})
 
 
