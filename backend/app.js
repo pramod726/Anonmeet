0ef07574a1a66cadb3e32e6a8aa78b7c18d3539a
Ayushman Tiwari <ayushman5022@gmail.com>
2024-03-09 03:28:55 +0530

    Socket done , Routes are left

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,11 @@
+import path from "path";
 const express = require("express");
 const dbconnect = require('./db/db_connect');
 import cookieParser from "cookie-parser";
-const app = express();
-
+import { app, server } from "./Socket/Socket.js";
 import authRoutes from "./routes/Auth.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-
-// it is used to import data from .env file
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
@@ -17,18 +15,18 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// connnecting database
-dbconnect();
 
-app.get("/", (req,res)=>{
-    return res.json({
-        success:true,
-        message:"Your server is up and running"
-    })
-})
 
-app.listen(PORT,()=>{
-    console.log(`App is running at port ${PORT}`)
-})
+const __dirname = path.resolve();
 
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
+
+server.listen(PORT, () => {
+	dbconnect();
+	console.log(`Server Running on port ${PORT}`);
+});
 
