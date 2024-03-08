c16b0b17b74147814f28bd375015b9d92fb00745
Ayushman Tiwari <ayushman5022@gmail.com>
2024-03-08 14:12:45 +0530

    bugg

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs";
-import User from "../models/user.model.js";
+import User from "../models/User.js";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
