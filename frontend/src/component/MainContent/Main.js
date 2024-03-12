52dc74f025c5533e5935d2721eb2f78df9a7bb7a
Anuj <tyagianuj234@gmail.com>
2024-03-13 01:33:49 +0530

    frontend updated

---
+++ frontend/src/component/MainContent/Main.js
@@ -0,0 +1,14 @@
+import Card from './Card'
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div className='ml-20'>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+  )
+}
