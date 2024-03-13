220dd9fe00587981590988596642a1127b28d453
Ayushman Tiwari <ayushman5022@gmail.com>
2024-03-14 03:49:45 +0530

    hooks and chat frontend

---
+++ frontend/src/component/Chat/ChatPage.jsx
@@ -0,0 +1,12 @@
+import MessageContainer from "../Chat/MessageContainer";
+import Sidebar from "./Sidebar/Sidebar";
+
+const Home = () => {
+	return (
+		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<Sidebar />
+			<MessageContainer />
+		</div>
+	);
+};
+export default Home;
