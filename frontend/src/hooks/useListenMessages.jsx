6c063d50456cf916750ef421a38ab8e637077e0e
Ayushman Tiwari <ayushman5022@gmail.com>
2024-03-14 20:18:47 +0530

    Login/Signup/Logout APIS and Context API added

--- frontend/src/hooks/useListenMessages.jsx
+++ frontend/src/hooks/useListenMessages.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
-import { useSocketContext } from "../context/SocketContext";
-import useConversation from "../zustand/useConversation";
+// import { useSocketContext } from "../context/SocketContext";
+import useConversation from "./useConversation";
 
 import notificationSound from "../assets/sounds/notification.mp3";
 
